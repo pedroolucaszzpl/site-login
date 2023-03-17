497277360a527bf30954e689e01d0721208ca9b2
pedroolucaszzpl <125031536+pedroolucaszzpl@users.noreply.github.com>
2023-03-17 14:35:02 -0300

    login

--- javascript/login.js
+++ javascript/login.js
@@ -3,9 +3,9 @@ function Logar(){
     var senha = '2023';
  
     if(document.getElementById('nome_login').value == usuario && document.getElementById('senha_login').value == senha){
-        console.log("Senha correta, entrando no sistema...")
+        alert("Senha correta, entrando no sistema...")
         window.location.href = ""
     }else{
-        console.log("Senha incorreta, tente novamente...")
+        alert("Senha incorreta, tente novamente...")
     }
  }
